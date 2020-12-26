b9ff8f65c113f650a80f53a603cd610da7ba189f
Fahmi Cog <fahmicog@gmail.com>
2020-12-26 23:26:29 +0700

    Update list.js
    N

--- 070/list.js
+++ 070/list.js
@@ -1,1 +1,1 @@
-var liSensi = ['fahmicog.site','fahmicog.blogspot.com','themeghosts.blogspot.com']
+eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('5 4=[\'3.1.0\',\'2.1.0\']',6,6,'com|blogspot|themeghosts|ghostheme|liSensi|var'.split('|'),0,{}))

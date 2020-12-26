3826977e6e128c384682e05ce2d4c752016f9521
Fahmi Cog <fahmicog@gmail.com>
2020-12-26 23:22:25 +0700

    u
    S

--- 070/list.js
+++ 070/list.js
@@ -1,1 +1,1 @@
-var liSensi = ['fahmicog.site','fahmicog.blogspot.com','themeghosts.blogspot.com']
+var liSensi = ['fahmicog.site','fahmicog.blogspot.com','ghostheme.blogspot.com','themeghosts.blogspot.com'];
